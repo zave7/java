7eae13b87b66807b83cfaef4e0303490baf50523
zave <zave7@naver.com>
2019-03-27 01:04:07 +0900

    JDBC basic setting

---
+++ javase/JDBC/src/com/kitri/jdbc/v1/BasicJDBC.java
@@ -0,0 +1,65 @@
+package com.kitri.jdbc.v1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BasicJDBC {
+	public static void main(String[] args) {
+        String DB_URL = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
+        String DB_USER = "scott";
+        String DB_PASSWORD = "tiger";
+
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        String query = "SELECT * FROM emp";
+        try {
+            // 드라이버를 로딩한다.
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e ) {
+            e.printStackTrace();
+        }
+
+        try {
+            // 데이터베이스의 연결을 설정한다.
+            conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            // Statement를 가져온다.
+            stmt = conn.createStatement();
+
+            // SQL문을 실행한다.
+            rs = stmt.executeQuery(query);
+
+            while (rs.next()) { 
+                String empno = rs.getString(1);
+                String ename = rs.getString(2);
+                String job = rs.getString(3);
+                String mgr = rs.getString(4);
+                String hiredate = rs.getString(5);
+                String sal = rs.getString(6);
+                String comm = rs.getString(7);
+                String depno = rs.getString(8);
+                // 결과를 출력한다.
+                System.out.println( 
+                    empno + " : " + ename + " : " + job + " : " + mgr
+                    + " : " + hiredate + " : " + sal + " : " + comm + " : "
+                + depno); 
+            }
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // ResultSet를 닫는다.
+                rs.close();
+                // Statement를 닫는다.
+                stmt.close();
+                // Connection를 닫는다.
+                conn.close();
+            } catch ( SQLException e ) {}
+        }
+    } // main()의 끝
+}
---
+++ javase/JDBC/src/com/kitri/jdbc/v2/UserDao.java
@@ -0,0 +1,56 @@
+package com.kitri.jdbc.v2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.kitri.jdbc.v1.User;
+
+public class UserDao {
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Connection c = getConnection();	// 중복 제거
+		
+		PreparedStatement ps = c.prepareStatement(
+				"insert into users(id, name, password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+		
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Connection c = getConnection(); // 중복 제거
+
+		
+		PreparedStatement ps = c.prepareStatement(
+				"insert into users(id, name, password) values(?,?,?)");
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+	}
+	
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection(
+				"jdbc:mysql://Localhost/springbook", "spring", "bool");
+		return c;
+	}
+}
