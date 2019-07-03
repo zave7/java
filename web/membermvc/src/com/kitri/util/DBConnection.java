f180a52b10bd666c4d87fc58da4acfc3642d1d9d
zave7 <zave7@naver.com>
2019-07-03 17:53:19 +0900

    finish cafe!

--- web/membermvc/src/com/kitri/util/DBConnection.java
+++ web/membermvc/src/com/kitri/util/DBConnection.java
@@ -2,17 +2,31 @@ package com.kitri.util;
 
 import java.sql.*;
 
+import javax.naming.*;
+import javax.sql.DataSource;
+
 public class DBConnection {
 	
-	static {
+//	static {
+//		try {
+//			Class.forName(SiteConstance.DB_DRIVER);
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	public static Connection makeConnection() throws SQLException {
+//		return DriverManager.getConnection(SiteConstance.DB_URL, SiteConstance.DB_USERNAME, SiteConstance.DB_PASSWORD);
+//	}
+	public static Connection makeConnection() throws SQLException {
 		try {
-			Class.forName(SiteConstance.DB_DRIVER);
-		} catch (ClassNotFoundException e) {
+			Context ictx = new InitialContext();
+			Context ctx = (Context) ictx.lookup("java:comp/env");
+			DataSource dataSource = (DataSource) ctx.lookup("jdbc/kitri");
+			return dataSource.getConnection();
+		} catch (NamingException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static Connection makeConnection() throws SQLException {
-		return DriverManager.getConnection(SiteConstance.DB_URL, SiteConstance.DB_USERNAME, SiteConstance.DB_PASSWORD);
+		return null;
 	}
 }
